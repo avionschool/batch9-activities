9f269504c83049ff7c17d13f3ea787a0a2f7a73c
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-04-27 03:28:38 +0800

    changed background, fixed some user story, modified some UI

--- dollosa-michael/survey-form/index.js
+++ dollosa-michael/survey-form/index.js
@@ -1,24 +1,21 @@
 console.log("test script");
+console.log(window.location.href)
 const name = document.getElementById("name");
 const submit = document.getElementById("submit")
 const form = document.getElementById("survey-form")
 const errMsg = document.getElementById("invalid-msg")
 const successMsg = document.getElementById("success-msg")
 const filler = document.getElementById("filler")
+
+
 submit.addEventListener('click', e => {
-        
-  console.log(form.checkValidity());
 
   if(!form.checkValidity()) {
     successMsg.style.display = "none"
     filler.style.display = "none"
     errMsg.style.display = "block"
   }
-
+  
 });
 
-submit.addEventListener('submit', e => {
-    errMsg.style.display = "none"
-    successMsg.style.display = "block"
-    e.preventDefault();
-})
+
