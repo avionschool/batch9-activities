d4a6b56270a4fd6d61183d46b4fcce1b50758bc1
Benson Miakoun <bmiakoun@gmail.com>
2021-04-30 09:35:44 +0800

    finished chessboard-flex project

---
+++ miakoun-benson/chessboard-flex/index.js
@@ -0,0 +1,113 @@
+function showMoves(event) {
+    const index = event.target.id;
+
+    switch(index) {
+        case "b5":
+            document.querySelector('#b5').classList.add('origin');
+            document.querySelector('#b5').classList.add('float');
+            document.querySelector('#body').classList.add('blur');
+            document.querySelector('#b13').classList.add('moves');
+            document.querySelector('#b21').classList.add('moves');
+            break;
+        case "b6":
+            document.querySelector('#b6').classList.add('origin');
+            document.querySelector('#b6').classList.add('float');
+            document.querySelector('#b13').classList.add('moves');
+            document.querySelector('#b20').classList.add('moves');
+            document.querySelector('#b27').classList.add('moves');
+            document.querySelector('#b34').classList.add('moves');
+            document.querySelector('#b41').classList.add('moves');
+            break;
+        case "b7":
+            document.querySelector('#b7').classList.add('origin');
+            document.querySelector('#b7').classList.add('float');
+            document.querySelector('#b13').classList.add('moves');
+            document.querySelector('#b22').classList.add('moves');
+            document.querySelector('#b24').classList.add('moves');
+            break;
+        case "b14":
+            document.querySelector('#b14').classList.add('origin');
+            document.querySelector('#b14').classList.add('float');
+            document.querySelector('#b22').classList.add('moves');
+            document.querySelector('#b30').classList.add('moves');
+            break;
+        case "b15":
+            document.querySelector('#b15').classList.add('origin');
+            document.querySelector('#b15').classList.add('float');
+            document.querySelector('#b23').classList.add('moves');
+            document.querySelector('#b31').classList.add('moves');
+            break;
+        case "b19":
+            document.querySelector('#b19').classList.add('origin');
+            document.querySelector('#b19').classList.add('float');
+            document.querySelector('#b2').classList.add('moves');
+            document.querySelector('#b13').classList.add('moves');
+            document.querySelector('#b25').classList.add('moves');
+            document.querySelector('#b34').classList.add('moves');
+            document.querySelector('#b36').classList.add('moves');
+            break;
+    }   
+}
+
+function hideMoves(event) {
+    const index = event.target.id;
+
+    switch(index) {
+        case "b5":
+            document.querySelector('#b5').classList.remove('origin');
+            document.querySelector('#b5').classList.remove('float');
+            document.querySelector('#body').classList.remove('blur');
+            document.querySelector('#b13').classList.remove('moves');
+            document.querySelector('#b21').classList.remove('moves');
+            break;
+        case "b6":
+            document.querySelector('#b6').classList.remove('origin');
+            document.querySelector('#b6').classList.remove('float');
+            document.querySelector('#b13').classList.remove('moves');
+            document.querySelector('#b20').classList.remove('moves');
+            document.querySelector('#b27').classList.remove('moves');
+            document.querySelector('#b34').classList.remove('moves');
+            document.querySelector('#b41').classList.remove('moves');
+            break;
+        case "b7":
+            document.querySelector('#b7').classList.remove('origin');
+            document.querySelector('#b7').classList.remove('float');
+            document.querySelector('#b13').classList.remove('moves');
+            document.querySelector('#b22').classList.remove('moves');
+            document.querySelector('#b24').classList.remove('moves');
+            break;
+        case "b14":
+            document.querySelector('#b14').classList.remove('origin');
+            document.querySelector('#b14').classList.remove('float');
+            document.querySelector('#b22').classList.remove('moves');
+            document.querySelector('#b30').classList.remove('moves');
+            break;
+        case "b15":
+            document.querySelector('#b15').classList.remove('origin');
+            document.querySelector('#b15').classList.remove('float');
+            document.querySelector('#b23').classList.remove('moves');
+            document.querySelector('#b31').classList.remove('moves');
+            break;
+        case "b19":
+            document.querySelector('#b19').classList.remove('origin');
+            document.querySelector('#b19').classList.remove('float');
+            document.querySelector('#b2').classList.remove('moves');
+            document.querySelector('#b13').classList.remove('moves');
+            document.querySelector('#b25').classList.remove('moves');
+            document.querySelector('#b34').classList.remove('moves');
+            document.querySelector('#b36').classList.remove('moves');
+            break;
+    }
+}
+
+const boxes = document.querySelectorAll('.box');
+
+if(boxes) {
+    counter = 1;
+    boxes.forEach(item => {
+        item.id = "b" + counter;
+        counter++;
+        item.addEventListener('mouseover', showMoves)
+        item.addEventListener('mouseout', hideMoves)
+    });
+}
